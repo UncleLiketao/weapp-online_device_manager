dcbdb1d2bcdc5074855835ea39c671fd211e75b4
unknown <unknown@example.com>
2020-09-09 23:57:26 +0800

    [ref] 重写设备借出逻辑
    修改原借出修改逻辑为扫码借出

--- pages/home/home.js
+++ pages/home/home.js
@@ -11,21 +11,40 @@ Page({
     returnIsDisabled: true,
     borrower: ""
   },
-  clickBorrow: function (e) {
-    if (!this.data.borrowIsDisabled) {
-      this.setData({
-        borrowIsDisabled: true, //修改isDisabled的值为true（即启用状态）
-        returnIsDisabled: false,
-        borrower: "李柯陶"
-      })
-    } else { //当disabled=true时
-      this.setData({
-        borrowIsDisabled: false, //修改isDisabled的值为false（即禁用状态）
-        returnIsDisabled: true,
-        borrower:""
-      })
-    }
+  deviceCodeScan: function (e) {
+    wx.scanCode({
+      scanType: ['qrCode'],
+      success: (res) => {
+        var str = res.result;
+        console.log(str)
+        if (str == "XGIMIH2LKT") {
+          wx.showModal({
+            title: '校验成功',
+            content: '点击确定输入你的名字',
+            confirmColor: '#9ca9e9',
+            success(res) {
+              if (res.confirm) {
+                console.log('用户点击确定')
+              } else if (res.cancel) {
+                console.log('用户点击取消')
+              }
+            }
+          });
+          return;
+
+        } else {
+          wx.showModal({
+            title: '借出失败',
+            content: '设备二维码不匹配',
+            showCancel: false,
+            confirmColor: '#9ca9e9'
+          });
+          return;
+        }
+      },
+    })
   },
+
 
   /**
    * 生命周期函数--监听页面加载
