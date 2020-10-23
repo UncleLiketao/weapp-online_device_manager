e108718e352f06b8a65115b26c8b8af7e1ed6915
Hello桃sir <414480664@qq.com>
2020-10-23 11:47:49 +0800

    [feat] 我的页面新增信息查看栏“我借出的设备”

---
+++ pages/my-borrowed-device/my-borrowed-device.js
@@ -0,0 +1,103 @@
+// pages/my-device/my-device.js
+// 初始化数据库
+const db = wx.cloud.database()
+const _ = db.command
+
+
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    deviceData: [],
+    userName:"",
+  },
+   /**
+   * 搜索函数
+   * 
+   */
+  loadUserBorrowedData: function () {
+    let self = this;
+    wx.getStorage({
+      key: 'user',
+      success: function (res) {
+        console.log(res.data["name"])
+        self.setData({
+          userName: res.data["name"]
+        })
+        db.collection('device_data')
+          .where({
+            borrowerName: self.data.userName,
+            owner:_.neq(self.data.userName)
+          })
+          .get({
+            success: function (res) {
+              console.log(res)
+              self.setData({
+                deviceData: res.data
+              })
+            }
+          })
+      }
+    })
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.loadUserBorrowedData()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
